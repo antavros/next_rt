3d1c266261478d53da1f13a50f8fdb1a2647f340
antavros <antavros@inbox.ru>
2024-07-10 05:27:06 +0300

    0.08 upd#1 fix#2

--- next.config.js
+++ next.config.js
@@ -1,34 +1,31 @@
 module.exports = {
-  experimental: {
-    reactCompiler: true,
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
       {
-        hostname: 'avatars.mds.yandex.net',
+        hostname: "avatars.mds.yandex.net",
       },
       {
-        hostname: 'imagetmdb.com',
+        hostname: "imagetmdb.com",
       },
       {
-        hostname: 'image.openmoviedb.com',
+        hostname: "image.openmoviedb.com",
       },
       {
-        hostname: 'st.kp.yandex.net',
+        hostname: "st.kp.yandex.net",
       },
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com'
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'avatar.vercel.sh'
+        protocol: "https",
+        hostname: "avatar.vercel.sh",
       },
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com'
-      }
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
-}
+};
