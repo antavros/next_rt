e87a75745f1b9c01d7a95bb287affd0c4bc58ffc
antavros <antavros@inbox.ru>
2024-07-10 10:43:51 +0300

    0.08 upd#1 fix#4

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const path = require("path");
+
 module.exports = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
