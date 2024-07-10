1af312df929480c4752b456924adc4da19515cda
antavros <antavros@inbox.ru>
2024-07-11 01:41:35 +0300

    0.08 upd#1 fix#13

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,9 +5,9 @@ import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
 
 import { SessionProvider } from 'next-auth/react';
-import ThemeProvider from '@/components/shared/context/Theme/index';
+import ThemeProvider from '../components/shared/context/Theme/index';
 
-import { Preloader } from "@/components/features/PreLoader";
+import { Preloader } from "../components/features/PreLoader";
 
 const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
