80b6d758d968e4565ca3d630773e4e4c94214c39
antavros <antavros@inbox.ru>
2024-08-16 16:42:35 +0300

    0.09 beta#2 fix#2

--- components/entities/User/Page/SignIn/index.tsx
+++ components/entities/User/Page/SignIn/index.tsx
@@ -14,7 +14,7 @@ function onChange(value: any) {
 }
 
 export function SignInPage() {
-
+  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_TOKEN as string;
   const { data: session } = useSession();
   const [login, setLogin] = useState("");
   const [email, setEmail] = useState("");
@@ -193,7 +193,7 @@ export function SignInPage() {
           </span>
         </div>
       ) : null}
-      <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_TOKEN} onChange={onChange} />
+      <ReCAPTCHA sitekey={siteKey} onChange={onChange} />
       <h1>или</h1>
       <button onClick={() => signIn("google")}>
         <Image
