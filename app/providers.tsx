26650cbfeabf28e068aff4b721de895540270f14
antavros <antavros@inbox.ru>
2025-05-12 08:20:22 +0300

    0.11 beta#7 fix#6

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,19 +4,19 @@ import React, { Suspense, StrictMode } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react";
 
-import ThemeProvider from "@/components/shared/context/theme";
+// import ThemeProvider from "@/components/shared/context/theme";
 import { SessionProvider } from "next-auth/react";
-import { PreLoader } from "@/components/widgets/preLoader";
-          // <Suspense fallback={<PreLoader />}>{children}</Suspense>
+// import { PreLoader } from "@/components/widgets/preLoader";
+// <Suspense fallback={<PreLoader />}>{children}</Suspense>
 
 const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <StrictMode>
       <SessionProvider>
-        <ThemeProvider>
-          {/* Обернули в Suspense с ленивым компонентом PreLoader */}
-          {children}
-        </ThemeProvider>
+        {/* <ThemeProvider> */}
+        {/* Обернули в Suspense с ленивым компонентом PreLoader */}
+        {children}
+        {/* </ThemeProvider> */}
         <SpeedInsights />
         <Analytics />
       </SessionProvider>
