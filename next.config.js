43aaa37dce5e56e38bb1c65ecd77cf9dca6adc54
antavros <antavros@inbox.ru>
2025-05-23 00:40:22 +0300

    0.11 beta#7 fix#19

--- next.config.js
+++ next.config.js
@@ -20,3 +20,49 @@ module.exports = {
     ],
   },
 };
+module.exports = {
+  reactStrictMode: true,
+  // experimental: {
+  //   reactCompiler: true,
+  // },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.mds.yandex.net",
+      },
+      {
+        protocol: "https",
+        hostname: "imagetmdb.com",
+      },
+      {
+        protocol: "https",
+        hostname: "image.openmoviedb.com",
+      },
+      {
+        protocol: "https",
+        hostname: "st.kp.yandex.net",
+      },
+      {
+        protocol: "https",
+        hostname: "yastatic.net",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatar.vercel.sh",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
